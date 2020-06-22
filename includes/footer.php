35e9ffb6b62d1063ab895d65bb7289a9d3d8770e
cesarep <cesar.e.p@hotmail.com>
2020-06-21 21:53:14 -0300

    Rodapé

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,10 @@
 </main>
     <footer class="container">
         <hr>
-        <p>&copy; 2020</p>
+        <p>
+        <span>Site desenvolvido por: <a href="mailto:cesar.e.p@hotmail.com">César E. Petersen</a> & <a href="mailto:rkingeski@hotmail.com">Rafael Kingeski</a></span>
+        <sp an class='float-right'>UDESC 2020</span>
+        </p>
     </footer>
     </body>
 </html>
